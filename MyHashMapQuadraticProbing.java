79516220724e3b40965490104a8219623c5def00
Jennifer Nghi Nguyen <jennifernghi@yahoo.com>
2016-05-07 23:43:31 -0700

    Update MyHashMapQuadraticProbing.java

--- MyHashMapQuadraticProbing.java
+++ MyHashMapQuadraticProbing.java
@@ -1,13 +1,12 @@
 import java.util.*;
 /**
- * 
- * @author Nghi Nguyen aka jennifernghi
+ * @author Nghi Nguyen
  *Implement MyMap using open addressing with quadratic probing) 
  *Create a new concrete class that implements MyMap using open addressing with quadratic probing. 
  *For simplicity, use f(key) = key % size as the hash function,
  * where size is the hash-table size. Initially, the hash-table size is 4. 
  * The table size is doubled whenever the load factor exceeds the threshold (0.5).
- * 
+ * Performing Quadratic Probing on put, remove, get in order to obtain O(1) for these methods
  */
 public class MyHashMapQuadraticProbing<V, K> implements MyMap<K, V> {
 
